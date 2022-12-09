8a135a725467c40a5e97033ad5e9a2fe87cf0a74
Obika Onyinyechi Viola <obikaviola2001@gmail.com>
2022-12-09 07:21:13 -0800

    modified files

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,7 +10,8 @@ int main(void)
 	{
 	printf("The size of a char data type is %d byte(s)\n", (int)sizeof(char));
 	printf("The size of an int data type is %d byte(s)\n", (int)sizeof(int));
+	printf("The size of a long int data type is %d bytes(s)\n", (int)sizeof(long int));
+	printf("The size of a long long int data type is %d byte(s)\n", (int)sizeof(long long int));
 	printf("The size of a float data type is %d byte(s)\n", (int)sizeof(float));
-	printf("The size of a double data type is %d byte(s)\n", (int)sizeof(double));
 	return (0);
 	}
