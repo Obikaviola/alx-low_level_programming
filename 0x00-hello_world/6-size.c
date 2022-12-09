a5a2f7f1f70ad2c67473c55b62fe6c0d8e48a3f4
Obika Onyinyechi Viola <obikaviola2001@gmail.com>
2022-12-09 07:32:36 -0800

    modified files

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,6 +12,6 @@ int main(void)
 	printf("Size of an int: %d byte(s)\n", (int)sizeof(int));
 	printf("Size of a long int: %d bytes(s)\n", (int)sizeof(long int));
 	printf("Size of a long long int: %d byte(s)\n", (int)sizeof(long long int));
-	printf("Size of a float: %d byte(s)\n", (int)sizeof(float));
+	printf("Size of a float: %d byte(s)", (int)sizeof(float));
 	return (0);
 	}
