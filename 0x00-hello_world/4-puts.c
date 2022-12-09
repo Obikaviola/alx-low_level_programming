dd9c8e36af62c4d7a7676f26c8eafda5f2c5419b
Obika Onyinyechi Viola <obikaviola2001@gmail.com>
2022-12-09 06:42:46 -0800

    added the 4-puts.c file

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main-Entry point
+ * Description:prints out the value stored in the variable using puts function
+ * Return:Always 0(Success)
+ */
+
+int main(void)
+	{
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return (0);
+	}
